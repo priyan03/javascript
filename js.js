1f0d8ddd6890e40535f9d42bf22c1c3bcb5d5c58
Priyan Paul Samuel P <96825787+priyan03@users.noreply.github.com>
2025-07-24 20:04:57 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,10 @@
-
+let ar = [1,2,2,3,5,7,5]
+ar.sort((a,b) => a - b) //1,2,2,3,5,5,7
+let duplicates = []
+for(let i = 0; i <= ar.length; i ++){
+        if(ar[i] !== ar[i + 1]){
+            duplicates.push(ar[i])
+        
+    }
+}
+console.log(duplicates)
